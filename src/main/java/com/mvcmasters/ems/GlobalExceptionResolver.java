a7a63528774b751f6538ae11eccaacf31304c6a9
nehCG <chen.guan.work@gmail.com>
2023-10-13 15:12:56 -0400

    feat(exception-handling): Add GlobalExceptionResolver

--- src/main/java/com/mvcmasters/ems/GlobalExceptionResolver.java
+++ src/main/java/com/mvcmasters/ems/GlobalExceptionResolver.java
@@ -1,2 +1,67 @@
-package com.mvcmasters.ems;public class GlobalExceptionResolver {
+package com.mvcmasters.ems;
+
+import com.mvcmasters.ems.base.ResultInfo;
+import com.mvcmasters.ems.exceptions.ParamsException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import com.alibaba.fastjson.JSON;
+
+@Component
+public class GlobalExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @Nullable Object handler,
+            @NonNull Exception ex) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("code", 500);
+        modelAndView.addObject("msg", "Server exception. Please try again...");
+
+        if (handler instanceof HandlerMethod handlerMethod) {
+            ResponseBody responseBody = handlerMethod.getMethod().getDeclaredAnnotation(ResponseBody.class);
+            if (responseBody == null) {
+                if (ex instanceof ParamsException p) {
+                    modelAndView.addObject("code", p.getCode());
+                    modelAndView.addObject("msg", p.getMsg());
+                }
+
+                return modelAndView;
+
+            } else {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setCode(500);
+                resultInfo.setMsg("Server exception. Please try again...");
+
+                if (ex instanceof ParamsException p) {
+                    resultInfo.setCode(p.getCode());
+                    resultInfo.setMsg(p.getMessage());
+                }
+
+                response.setContentType("application/json;charset=UTF-8");
+                try (PrintWriter out = response.getWriter()) {
+                    String json = JSON.toJSONString(resultInfo);
+                    out.write(json);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+
+            }
+        }
+
+        return modelAndView;
+    }
 }
