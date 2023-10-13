83130e723cb74a3bfe9dd87923926e772aefc576
nehCG <chen.guan.work@gmail.com>
2023-10-13 15:57:30 -0400

    feat(user-query): Add user query functionality

--- src/main/java/com/mvcmasters/ems/base/BaseService.java
+++ src/main/java/com/mvcmasters/ems/base/BaseService.java
@@ -1,9 +1,13 @@
 package com.mvcmasters.ems.base;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class BaseService<T,ID> {
 
@@ -14,4 +18,14 @@ public abstract class BaseService<T,ID> {
         return baseMapper.selectByParams(baseQuery);
     }
 
+    public Map<String, Object> queryByParamsForTable(BaseQuery baseQuery) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        PageHelper.startPage(baseQuery.getPage(), baseQuery.getLimit());
+        PageInfo<T> pageInfo = new PageInfo<T>(selectByParams(baseQuery));
+        result.put("count", pageInfo.getTotal());
+        result.put("data", pageInfo.getList());
+        result.put("code", 0);
+        result.put("msg", "");
+        return result;
+    }
 }
--- src/main/java/com/mvcmasters/ems/controller/UserController.java
+++ src/main/java/com/mvcmasters/ems/controller/UserController.java
@@ -4,12 +4,15 @@ import com.mvcmasters.ems.base.BaseController;
 import com.mvcmasters.ems.base.ResultInfo;
 import com.mvcmasters.ems.exceptions.ParamsException;
 import com.mvcmasters.ems.model.UserModel;
+import com.mvcmasters.ems.query.UserQuery;
 import com.mvcmasters.ems.service.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("user")
@@ -38,6 +41,12 @@ public class UserController extends BaseController {
         userService.updatePassWord(userId, oldPassword, newPassword, repeatPassword);
 
         return resultInfo;
+    }
+
+    @RequestMapping("list")
+    @ResponseBody
+    public Map<String, Object> selectByParams(UserQuery userQuery) {
+        return userService.queryByParamsForTable(userQuery);
     }
 
 }
