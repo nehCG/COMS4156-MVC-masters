c7ba66ccefcdd0ebe4f0da337d1a263455cfd72b
nehCG <chen.guan.work@gmail.com>
2023-10-13 18:25:26 -0400

    chore(db-update): Update user table

--- src/main/resources/ems.sql
+++ src/main/resources/ems.sql
@@ -14,10 +14,11 @@ CREATE TABLE `t_user` (
     `true_name` varchar(20) DEFAULT NULL,
     `email` varchar(30) DEFAULT NULL,
     `phone` varchar(20) DEFAULT NULL,
+    `is_valid` int(0) NULL DEFAULT 1,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (1, 'admin', '4QrcOUm6Wau+VuBX8g+IPg==', 'admin', 'admin@ems.com', '1111111111');
+INSERT INTO `t_user` VALUES (1, 'admin', '4QrcOUm6Wau+VuBX8g+IPg==', 'admin', 'admin@ems.com', '1111111111', 1);
