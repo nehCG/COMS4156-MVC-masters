a01453af3537527e8de57347af8eba651ddb73fd
Xinyun-123 <57038275+Xinyun-123@users.noreply.github.com>
2023-10-15 23:24:22 -0400

    chore(db-update): Add sql query to add shared space table (#5)
    * chore(db-update): Add t_sharedSpace table
    
    * fix(db-update): Fix table name in comment
    
    ---------
    
    Co-authored-by: Diane <diane@pietrastudio.com>

--- src/main/resources/ems.sql
+++ src/main/resources/ems.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA ems;
+# CREATE SCHEMA ems;
 
 USE ems;
 
@@ -22,3 +22,26 @@ CREATE TABLE `t_user` (
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES (1, 'admin', '4QrcOUm6Wau+VuBX8g+IPg==', 'admin', 'admin@ems.com', '1111111111', 1);
+
+
+
+
+-- ----------------------------
+-- Table structure for t_sharedSpace
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sharedSpace`;
+
+CREATE TABLE `t_sharedSpace` (
+                          `id` int NOT NULL AUTO_INCREMENT,
+                          `uid` int DEFAULT NULL REFERENCES t_user(id),
+                          `last_modified_by` int DEFAULT NULL REFERENCES t_user(id),
+                          `content` varchar(200) DEFAULT NULL,
+                          `created_time` timestamp DEFAULT NULL,
+                          `modified_time` timestamp DEFAULT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_sharedSpace
+-- ----------------------------
+INSERT INTO `t_sharedSpace` VALUES (1, 1, 1, 'First shared space!', TIMESTAMP('2022-05-08 14:30:45'), TIMESTAMP('2022-05-08 14:30:45'));
