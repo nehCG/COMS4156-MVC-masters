3846b4e8fb28d8af70e4663a5bd680c736c416c4
高千浛 <gaoqianhan@gaoqianandembp2.mynetworksettings.com>
2023-10-19 10:48:32 -0400

    style(shared-space): add javadocs and style check

--- src/main/java/com/mvcmasters/ems/repository/SharedDataMapper.java
+++ src/main/java/com/mvcmasters/ems/repository/SharedDataMapper.java
@@ -4,21 +4,40 @@ import com.mvcmasters.ems.base.BaseMapper;
 import com.mvcmasters.ems.model.SharedDataModel;
 
 import java.util.List;
-
+/**
+ * Mapper interface for shared data related database operations.
+ */
 public interface SharedDataMapper extends BaseMapper<SharedDataModel, Integer> {
-
-    // Insert a new shared data entry
+    /**
+     * Added a new shared data into the database.
+     *
+     * @param data the new shared data to be added.
+     */
     void insertSharedData(SharedDataModel data);
-
-    // Retrieve a shared data entry by its id
+    /**
+     * Queries the database to retrieve a shared data based on its ID.
+     *
+     * @param id the shared data id
+     * @return the Shared Data corresponding to the provided id,
+     * or null if not found.
+     */
     SharedDataModel selectSharedDataById(Integer id);
-
-    // Retrieve all shared data entries
+    /**
+     * Retrieve all shared data entries.
+     *
+     * @return all the shared data in the database.
+     */
     List<SharedDataModel> selectAllSharedData();
-
-    // Update a shared data entry by its id
+    /**
+     * Update a shared data entry by its id.
+     *
+     * @param data data content to be updated
+     */
     void updateSharedData(SharedDataModel data);
-
-    // Delete a shared data entry by its id
+    /**
+     * Delete a shared data entry by its id.
+     *
+     * @param id data id to be deleted
+     */
     void deleteSharedDataById(Integer id);
 }
--- src/main/java/com/mvcmasters/ems/service/SharedDataService.java
+++ src/main/java/com/mvcmasters/ems/service/SharedDataService.java
@@ -11,93 +11,138 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Service layer for shared data related operations.
+ */
 @Service
 public class SharedDataService extends BaseService<SharedDataModel, Integer> {
-
+    /**
+     * Mapper for shared data-related database operations.
+     */
     @Autowired
     private SharedDataMapper sharedDataMapper;
 
-    // Add a new shared data entry
-    public void addSharedData(SharedDataModel sharedData) {
+    /**
+     * Handles adding new shared data logic.
+     *
+     * @param sharedData the shared data to be added.
+     */
+    public void addSharedData(final SharedDataModel sharedData) {
         sharedData.setCreatedTime(LocalDateTime.now());
         validateSharedData(sharedData);
         sharedDataMapper.insertSharedData(sharedData);
     }
 
-    // Get a shared data entry by its id
-    public SharedDataModel getSharedDataById(Integer id) {
+    /**
+     * Retrieve a shared data by its id.
+     *
+     * @param id shared data id.
+     * @return a shared data with the corresponding id.
+     */
+    public SharedDataModel getSharedDataById(final Integer id) {
         SharedDataModel data = sharedDataMapper.selectSharedDataById(id);
         if (data == null) {
-            throw new CustomException("Record ID does not exist", HttpStatus.BAD_REQUEST);
+            throw new CustomException("Record ID does not exist",
+                                      HttpStatus.BAD_REQUEST);
         }
         return data;
     }
 
-    // View all shared data entries
+    /**
+     * Retrieve all shared data in the database.
+     *
+     * @return all shared data in the database.
+     */
     public List<SharedDataModel> getAllSharedData() {
         return sharedDataMapper.selectAllSharedData();
     }
 
-    // Update a shared data entry by its id
-    public void updateSharedData(Integer id, SharedDataModel newData) {
-        SharedDataModel existingData = sharedDataMapper.selectSharedDataById(id);
+    /**
+     * Updated a shared data by its id.
+     *
+     * @param id shared data id.
+     * @param newData data content to be updated.
+     */
+    public void updateSharedData(final Integer id,
+                                 final SharedDataModel newData) {
+        SharedDataModel existingData =
+                sharedDataMapper.selectSharedDataById(id);
         if (existingData == null) {
-            throw new CustomException("Record ID does not exist", HttpStatus.BAD_REQUEST);
+            throw new CustomException("Record ID does not exist",
+                    HttpStatus.BAD_REQUEST);
         }
 
         if (newData.getContent() == null && newData.getSubject() == null) {
-            throw new CustomException("Updates can not be null", HttpStatus.BAD_REQUEST);
+            throw new CustomException("Updates can not be null",
+                    HttpStatus.BAD_REQUEST);
         }
 
         // Check if the user ID is not null
         if (newData.getUid() == null) {
-            throw new CustomException("User ID cannot be null", HttpStatus.BAD_REQUEST);
+            throw new CustomException("User ID cannot be null",
+                    HttpStatus.BAD_REQUEST);
         }
 
         newData.setModifiedTime(LocalDateTime.now());
         newData.setId(id);
         sharedDataMapper.updateSharedData(newData);
-
-        // may need to add check for data owner match current logged-in user
     }
 
-    // Delete a shared data entry by its id
-    public void deleteSharedDataById(Integer id) {
+    /**
+     * Delete a shared data by its id.
+     *
+     * @param id shared data id.
+     */
+    public void deleteSharedDataById(final Integer id) {
         SharedDataModel data = sharedDataMapper.selectSharedDataById(id);
         // check if id exists in database
         if (data == null) {
-            throw new CustomException("Record ID does not exist", HttpStatus.BAD_REQUEST);
+            throw new CustomException("Record ID does not exist",
+                    HttpStatus.BAD_REQUEST);
         }
         sharedDataMapper.deleteSharedDataById(id);
 
         // may need to add check for data owner or only admin can delete
     }
 
-    public void validateSharedData(SharedDataModel sharedData){
+    /**
+     * Validate a shared data when adding a new data.
+     *
+     * @param sharedData shared data to be added.
+     */
+    public void validateSharedData(final SharedDataModel sharedData) {
+
         // Check if the sharedData object itself is not null
         if (sharedData == null) {
-            throw new CustomException("Shared data cannot be null", HttpStatus.BAD_REQUEST);
+            throw new CustomException("Shared data cannot be null",
+                    HttpStatus.BAD_REQUEST);
         }
 
-        // Check if the shared data object has a ID, which should be empty as the service
-        // will automatically assign ID
+        // Check if the shared data object has ID, which should be
+        // empty as the service will automatically assign ID
         if (sharedData.getId() != null) {
-            throw new CustomException("ID will be automatically assigned", HttpStatus.BAD_REQUEST);
+            throw new CustomException("ID will be automatically assigned",
+                    HttpStatus.BAD_REQUEST);
         }
 
         // Check if the user ID is not null
         if (sharedData.getUid() == null) {
-            throw new CustomException("User ID cannot be null", HttpStatus.BAD_REQUEST);
+            throw new CustomException("User ID cannot be null",
+                    HttpStatus.BAD_REQUEST);
         }
 
         // Check if the 'subject' field is not null
-        if (sharedData.getSubject() == null || sharedData.getSubject().isBlank()) {
-            throw new CustomException("Subject cannot be empty", HttpStatus.BAD_REQUEST);
+        if (sharedData.getSubject() == null
+                || sharedData.getSubject().trim().isBlank()) {
+            throw new CustomException("Subject cannot be empty",
+                    HttpStatus.BAD_REQUEST);
         }
 
         // Check if the 'content' field is not null
-        if (sharedData.getContent() == null || sharedData.getContent().isBlank()) {
-            throw new CustomException("Content cannot be empty", HttpStatus.BAD_REQUEST);
+        if (sharedData.getContent() == null
+                || sharedData.getContent().trim().isBlank()) {
+            throw new CustomException("Content cannot be empty",
+                    HttpStatus.BAD_REQUEST);
         }
     }
 
